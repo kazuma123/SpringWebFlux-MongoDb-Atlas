e51b5690758a8585c58729d439c51fb1ae068c02
Eddu <echambir@unsa.edu.pe>
2023-05-18 20:14:01 -0500

    commit

---
+++ viajes/src/main/java/com/wepapp/viajes/ViajesApplication.java
@@ -0,0 +1,13 @@
+package com.wepapp.viajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesApplication.class, args);
+	}
+
+}
---
+++ viajes/src/test/java/com/wepapp/viajes/ViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wepapp.viajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
